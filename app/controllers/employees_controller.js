1a77f47958ee03a2b7d1cc42ad42c02dd37f48b3
Johny Prieto <johnyprieto001@gmail.com>
2023-01-17 20:56:51 -0500

    add method to create a employee

--- app/controllers/employees_controller.js
+++ app/controllers/employees_controller.js
@@ -1,8 +1,8 @@
-
+const { StatusCodes }  = require('http-status-codes');
 const data = require('../../infraestructure/data/employees');
 const responseModel = require('../helpers/response');
 
-const getEmployeesList = async() => {
+const getEmployeesFn = async() => {
     let response = null;
     try {
         response = responseModel(true, data);
@@ -18,4 +18,23 @@ const getEmployeesList = async() => {
     return response;
 }
 
-module.exports = getEmployeesList
+const createEmployeesFn = async(data) => {
+    let response = null;
+    try {
+        response = responseModel(true, data, 'Empleado creado !', StatusCodes.CREATED);
+    } catch (error) {
+        response = responseModel(
+            false,
+            null,
+            `Error unexpected: ${error.message}`,
+            error.statusCode
+        );
+    }
+
+    return response;
+}
+
+module.exports = {
+    getEmployeesFn,
+    createEmployeesFn
+}
--- infraestructure/handlers/employees_handler.js
+++ infraestructure/handlers/employees_handler.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const responseHttp = require('../helpers/response');
-const getEmployeesList = require('../../app/controllers/employees_controller');
+const {getEmployeesFn, createEmployeesFn} = require('../../app/controllers/employees_controller');
 
 module.exports.getEmployees = async (event) => {
-  const {success, data, message, statusCode} = await getEmployeesList();
+  const {success, data, message, statusCode} = await getEmployeesFn();
+  return responseHttp(success, data, message, statusCode);
+};
+
+module.exports.createEmployees = async (event) => {
+  const {success, data, message, statusCode} = await createEmployeesFn(JSON.parse(event.body));
   return responseHttp(success, data, message, statusCode);
 };
