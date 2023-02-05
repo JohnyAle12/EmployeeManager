7bf3805da8ecbe45b8e8bb1b8ecfb4438e5d43f0
Johny Prieto <johnyprieto001@gmail.com>
2023-02-04 19:35:50 -0500

    add environment variables

--- infraestructure/repositories/connection.js
+++ infraestructure/repositories/connection.js
@@ -1,11 +1,11 @@
 const db = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'employee-manager.ce6utlo5cdri.us-east-2.rds.amazonaws.com',
-      port : 3306,
-      user : 'admin',
-      password : 'alejo123pv',
-      database : 'serverless'
+      host : process.env.MYSQL_HOST,
+      port : process.env.MYSQL_PORT,
+      user : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE
     }
 });
 
